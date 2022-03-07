59258c5d678f8edae4e871366679d62bf4e672d8
GP <ganesh.prasannah@oslash.com>
2022-03-07 17:04:57 +0530

    fix(core): do a cleaner fix for the defualt exports in CJS problem

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,12 +7,13 @@ export default defineConfig({
   minify: true,
   clean: true,
   esbuildOptions: (options) => {
+    // eslint-disable-next-line no-param-reassign
     options.footer = {
       // This will ensure we can continue writing this plugin
       // as a modern ECMA module, while still publishing this as a CommonJS
       // library with a default export, as that's how ESLint expects plugins to look.
       // @see https://github.com/evanw/esbuild/issues/1182#issuecomment-1011414271
-      js: "module.exports = module.exports.default;",
+      js: 'module.exports = module.exports.default;',
     }
   },
   entry: ['src/index.ts'],
